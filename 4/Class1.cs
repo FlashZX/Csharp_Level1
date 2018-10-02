46a82df1b4c2c0b6cf11ab6e4295e1b0f90cdea5
FlashZX <flashikk@inbox.ru>
2018-10-02 16:45:42 +0500

    ДЗ 4

---
+++ 4/Class1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HomeWork_4
+{
+    class MyArray
+    {
+        int[] a;
+        public int MaxCount { get; set; }
+
+        /// <summary>
+        /// Создаем целочисленный массив, заполняющийся числами от начального значения с заданным шагом.
+        /// </summary>
+        /// <param name="n">количество элементов</param>
+        /// <param name="start">значение 0 элемента массива</param>
+        /// <param name="step">шаг изменения каждого последующего значения</param>
+        public MyArray(int n, int start, int step)
+        {
+            a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = start + step * i;
+            }
+        }
+
+        /// <summary>
+        /// Читаем массив из файла
+        /// </summary>
+        /// <param name="filename">имя файла .txt</param>
+        public MyArray(string filename)
+        {
+            //Если файл существует
+            if (File.Exists(filename))
+            {
+                //Считываем все строки из файла
+                string[] ss = File.ReadAllLines(filename);
+                a = new int[ss.Length];
+                //Переводим данные из строкового формата в числовой
+                for (int i = 0; i < ss.Length; i++)
+                    a[i] = int.Parse(ss[i]);
+            }
+            else Console.WriteLine("Error load file");
+        }
+
+        /// <summary>
+        /// Свойство для расчета суммы всех элементов целочисленного массива
+        /// </summary>
+        public int Sum
+        {
+            get
+            {
+                int sum = 0;
+                for (int i = 0; i < a.Length; i++)
+                {
+                    sum += a[i];
+                }
+                return sum;
+            }
+        }
+
+        /// <summary>
+        /// Метод меняющий знаки элементов массива
+        /// </summary>
+        public void Inverse()
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] *= -1;
+            }
+        }
+
+        /// <summary>
+        /// Метод умножающий все элементы массива на число
+        /// </summary>
+        /// <param name="x">множитель</param>
+        public void Multi(int x)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = -a[i];
+            }
+        }
+
+        /// <summary>
+        /// Свойство, показывающие количество элементов массива с максимальным значением
+        /// </summary>
+        public int MaxCounter(int[] arr)
+        {
+            int max = arr.Max();
+            int s = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == max)
+                    s += 1;
+            }
+            return s;
+        }
+
+        /// <summary>
+        /// Выводим на консоль все элементы массива в одной строке
+        /// </summary>
+        public void Print()
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write("{0} ", a[i]);
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Выводит количеств пар элементов массива, делящихся на n без остатка
+        /// </summary>
+        /// <param name="n">делитель</param>
+        /// <returns>количество пар элементов массива</returns>
+        public int Pair_to_N(int n)
+        {
+            int count = 0;
+            for (int i = 0; i < (a.Length - 1); i++)
+            {
+                if ((a[i] % n == 0) || (a[i + 1] % n == 0))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        /// <summary>
+        /// Свойство для записи в файл
+        /// </summary>
+        /// <param name="filename"></param>
+        public void Rec(string filename)
+        {
+            //переводим данные из чисел в строки
+            string[] a_string = new string[a.Length];
+            for (int i = 0; i < a_string.Length; i++)
+                a_string[i] = Convert.ToString(a[i]);
+
+            //пишем массив со строками в файл
+            System.IO.File.WriteAllLines(filename, a_string);
+        }
+    }
+
+}
---
+++ 4/Program.cs
@@ -0,0 +1,128 @@
+﻿//Насыров Игорь. г.Пермь. Домашка № 4.
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HomeWork_4
+{
+    
+    class Program
+    {
+        /// <summary>
+        /// Метод считывания из файла txt
+        /// </summary>
+        /// <param name="filename">имя файла .txt</param>
+        static void Read(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                //Считываем все строки из файла
+                string[] ss = File.ReadAllLines(filename);
+                for (int i = 0; i < ss.Length; i++)
+                {
+                    Console.Write(ss[i] + " ");
+                }
+                Console.WriteLine();
+            }
+            else Console.WriteLine("Error load file");
+        }
+
+        static void Main(string[] args)
+        {
+            #region Задание 1 (Пары эллементов)
+            Random rnd = new Random();
+            int[] Array1 = new int[20];
+            int count = 0;
+
+            for (int i = 0; i < Array1.Length; i++) Array1[i] = rnd.Next(-10000, 10001);
+            for (int i = 0; i < Array1.Length - 1; i++)
+            {
+                if (Array1[i] % 3 == 0 || Array1[i + 1] % 3 == 0)
+                {
+                    count++;
+                    Console.WriteLine("Пара числел - {0} и {1}", Array1[i], Array1[i + 1]);
+                }
+            }
+            Console.WriteLine("Колличечтво пар элементов - " + count);
+            Console.ReadKey();
+            Console.Clear();
+            #endregion
+
+            #region Задание 2а
+            MyArray array1 = new MyArray(15, 0, 2);
+            array1.Print();
+
+            Console.WriteLine("Сумма: " + array1.Sum);
+
+            array1.Inverse();
+            Console.Write("Инверсия: ");
+            array1.Print();
+
+            array1.Multi(3);
+            Console.Write("Умножаем на 3: ");
+            array1.Print();
+            Console.WriteLine("Количество максимальных элементов в массиве array1: " + array1.MaxCount);
+            Console.ReadKey();
+            Console.Clear();
+            #endregion
+
+            #region Задание 2б
+            MyArray array2 = new MyArray(@"d:\test.txt");
+
+            Console.WriteLine("Читаем из файла");
+            array2.Print();
+            Console.WriteLine("Пишем в файл массив array1 и читаем файл с новыми данными");
+            array1.Rec(@"d:\test.txt");
+            Read(@"d:\test.txt");
+
+            Console.ReadKey();
+            Console.Clear();
+            #endregion
+
+            #region Задание 3 (в структуру уже не успел)
+            //Считываем все строки из файла
+            int attempts = 4;
+            bool passing;
+
+            string ulogin;
+            string upassword;
+
+            StreamReader sr = new StreamReader(@"d:\account.txt");
+            string login = sr.ReadLine();
+            string password = sr.ReadLine();
+
+            do
+            {
+                Console.Write("Введите логин: ");
+                ulogin = Console.ReadLine();
+                Console.Write("Введите пароль: ");
+                upassword = Console.ReadLine();
+                attempts--;
+
+                if (ulogin == login && upassword == password)
+                {
+                    passing = true;
+                }
+                else passing = false;
+
+                if (passing == true)
+                    Console.WriteLine("Добро пожаловать!");
+                else
+                    Console.WriteLine("Логин или пароль, введен не верно!");
+                Console.ReadKey();
+                Console.Clear();
+            } while (attempts > 1);
+
+            Console.WriteLine("Вы не прошли проверку, прощайте!");
+            Console.ReadKey();
+            Console.Clear();
+            #endregion
+        }
+
+
+    }
+}
