c297d60e25afb9bd117a6f897d6e985db2e7011c
FlashZX <flashikk@inbox.ru>
2018-10-16 16:08:29 +0500

    ДЗ 8

---
+++ 8/Program.cs
@@ -0,0 +1,24 @@
+﻿//Насыров Игорь, г.Пермь, ДЗ №8
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Be_OrNot_toBe
+
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
